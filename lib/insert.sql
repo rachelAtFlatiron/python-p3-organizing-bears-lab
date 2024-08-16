5afece6f5bfbc7f1f478685b4e675838e9de30cc
Rachel Katz <rachel.katz@flatironschool.com>
2024-08-16 12:03:58 -0400

    demo done

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 5, 'M', 'Brown', 'Aggressive', TRUE),
+('Rowdy', 3, 'F', 'Black', 'Calm', TRUE),
+('Tabitha', 7, 'F', 'Cinnamon', 'Curious', FALSE),
+('Sergeant Brown', 2, 'M', 'Black', 'Playful', TRUE),
+('Melissa', 4, 'M', 'Brown', 'Friendly', TRUE),
+('Grinch', 6, 'F', 'White', 'Shy', TRUE),
+('Wendy', 8, 'M', 'Gray', 'Aggressive', TRUE);
+
+INSERT INTO bears (age, sex, color, temperament, alive) VALUES
+(1, 'F', 'Brown', 'Energetic', TRUE);
